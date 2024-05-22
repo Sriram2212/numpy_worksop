4357f4e80054d3150927529e2b45b5d4cd9fd127
SRIRAM M <155570179+Sriram2212@users.noreply.github.com>
2024-05-22 11:20:16 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,1 +1,9 @@
 #write a program to find the sum of digits of a given number'
+num=int(input("Enter a number:"))
+sum=0
+for i in range(0,num):
+ if(num>0):
+  r=num%10
+  sum=sum+r
+  num=num//10
+print(sum)
